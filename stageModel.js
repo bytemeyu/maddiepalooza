9f3a8c5ce33beeb49091b542d6ac7950dbcbfbe0
bytemeyu <bytemeyu@gmail.com>
2024-05-20 20:32:55 -0300

    corrigindo artistModel.js

--- stageModel.js
+++ stageModel.js
@@ -1,8 +1,16 @@
-export class Stage {
-    constructor(stage_id, name, location, capacity) {
-        this.stage_id = stage_id;
-        this.name = name;
-        this.location = location;
-        this.capacity = capacity;
-    }
-}
+export const stageModel = {
+    tableName: 'stage',
+    columns: {
+        stage_id: { type: 'integer', allowNull: false, default: 'nextval(\'stage_stage_id_seq\'::regclass)' },
+        name: { type: 'character varying(50)', allowNull: true },
+        location: { type: 'character varying(50)', allowNull: false },
+        capacity: { type: 'integer', allowNull: false },
+    },
+    primaryKey: 'stage_id',
+    indexes: [
+        { name: 'stage_pkey', type: 'PRIMARY KEY', columns: ['stage_id'] },
+    ],
+    foreignKeyConstraints: [
+        { name: 'performance_stage_id_fkey', foreignKey: 'stage_id', references: { table: 'stage', column: 'stage_id' } },
+    ],
+};
