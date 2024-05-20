bfe1936dfaa7b6756daae08b9e109010ab467128
bytemeyu <bytemeyu@gmail.com>
2024-05-20 20:16:36 -0300

    corrigindo e completando artistModel.js

--- artistModel.js
+++ artistModel.js
@@ -1,8 +1,17 @@
-export class Artist {
-    constructor(artist_id, name, biography, photo_url) {
-        this.artist_id = artist_id;
-        this.name = name;
-        this.biography = biography;
-        this.photo_url = photo_url;
-    }
-}
+export const artistModel = {
+    tableName: 'artist',
+    columns: {
+        artist_id: { type: 'integer', allowNull: false, default: 'nextval(\'artists_artist_id_seq\'::regclass)' },
+        name: { type: 'character varying(50)', allowNull: false },
+        biography: { type: 'character varying(400)', allowNull: false },
+        photo_url: { type: 'character varying(400)', allowNull: false },
+    },
+    primaryKey: 'artist_id',
+    indexes: [
+        { name: 'artists_pkey', type: 'PRIMARY KEY', columns: ['artist_id'] },
+        { name: 'idx_artist_name', type: 'INDEX', columns: ['name'] },
+    ],
+    referencedBy: [
+        { tableName: 'performance', foreignKey: 'artist_id', referencingTable: 'artist', referencingKey: 'artist_id' },
+    ],
+  };
