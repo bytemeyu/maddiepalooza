14f78883cb21479ed44179dd34a7aae70f2a2a78
bytemeyu <bytemeyu@gmail.com>
2024-05-24 13:09:36 -0300

    corrigindo artistRepository.js

--- src/repositories/artistRepository.js
+++ src/repositories/artistRepository.js
@@ -2,11 +2,11 @@ import { query } from '../config/database.js';
 
 export const artistRepository = {
     getAllArtists: async() => {
-        const text = 'SELECT * FROM artists';
+        const text = 'SELECT * FROM artist';
         try {
             const { rows } = await query(text);
             return rows;
-        } catch (err) {
+        } catch(err) {
             console.error(`Erro ao recuperar todos os artistas: ${err.message}`);
             throw err;
         }
