4a02f2de1e2336a6881f4af0d50355f9d7e4362e
bytemeyu <bytemeyu@gmail.com>
2024-05-25 12:08:23 -0300

    atualizando artistRoutes.js com a rota getArtistById

--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -4,5 +4,6 @@ import { artistController } from '../controllers/artistController.js';
 const router = express.Router();
 
 router.get('/', artistController.getAllArtists);
+router.get('/:id', artistController.getArtistById);
 
 export default router;
