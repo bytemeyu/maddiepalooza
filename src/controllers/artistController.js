6722e57e65d087fcd08c54791c67f9ef42e92fdc
bytemeyu <bytemeyu@gmail.com>
2024-05-25 12:09:38 -0300

    atualizando respostas getAllArtists e criando getArtistById no artistController.js

--- src/controllers/artistController.js
+++ src/controllers/artistController.js
@@ -4,18 +4,60 @@ export const artistController = {
     getAllArtists: async(req, res) => {
         try {
             const allArtists = await artistService.getAllArtists();
-            res.status(200).json(
-                {
-                    'success': true, 	
-                    'data': allArtists,
-                }
-            );
+            console.log(`tipo de allArtists: ${typeof allArtists}`);
+            console.log(`allArtists é um objeto do tipo array? ${Array.isArray(allArtists)}`);
+            if(allArtists.length === 0) {
+                res.status(200).json(
+                    {
+                        'success': true,
+                        'data': 'Não há nenhum artista no banco de dados'
+                    }
+                );
+            } else {
+                res.status(200).json(
+                    {
+                        'success': true, 	
+                        'data': allArtists,
+                    }
+                );
+            }
         } catch(err) {
             console.error(`Erro ao recuperar todos os artistas: ${err.message}`);
             res.status(500).json(
                 {
                     'success': false, 	
                     'error': 'Erro ao recuperar todos os artistas',
+                }
+            );
+        }
+    },
+
+    getArtistById: async(req, res) => {
+        const { id } = req.params;
+        console.log(`id extraído da requisição: ${id}`);
+        try {
+            const artistById = await artistService.getArtistById(id);
+            if(artistById.length === 0){
+                res.status(200).json(
+                    {
+                        'sucess': true,
+                        'data': 'Não há nenhum artista com o id especificado no banco de dados'
+                    }
+                );
+            } else {
+                res.status(200).json(
+                    {
+                        'success': true,
+                        'data': artistById,
+                    }
+                );
+            }
+        } catch(err) {
+            console.error(`Erro ao recuperar artista com id ${id}: ${err.message}`);
+            res.status(500).json(
+                {
+                    'success': false,
+                    'error': 'Erro ao recuperar artista a partir de id',
                 }
             );
         }
