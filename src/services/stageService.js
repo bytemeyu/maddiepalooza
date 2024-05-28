e5b70d6b9053847883adfb996ea67f3cde9d6e23
bytemeyu <bytemeyu@gmail.com>
2024-05-28 19:31:51 -0300

    criando e implementando métodos stageService.js

---
+++ src/services/stageService.js
@@ -0,0 +1,48 @@
+import { stageRepository } from '../repositories/stageRepository.js';
+
+export const stageService = {
+    getAllStages: async() => {
+        try {
+            return await stageRepository.getAllStages();
+        } catch(err) {
+            console.error(`Erro ao recuperar todos os palcos: ${err.message}`);
+            throw err;
+        }
+    },
+
+    getStageById: async(id) => {
+        try {
+            return await stageRepository.getStageById(id);
+        } catch(err) {
+            console.error(`Erro ao recuperar palco com id ${id}: ${err.message}`);
+            throw err;
+        }
+    },
+
+    createStage: async(name, location, capacity) => {
+        try {
+            return await stageRepository.createStage(name, location, capacity);
+        } catch(err) {
+            console.error(`Erro ao criar novo palco no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    updateStage: async(id, name, location, capacity) => {
+        try {
+            return await stageRepository.updateStage(id, name, location, capacity);
+        } catch(err) {
+            console.error(`Erro ao atualizar palco com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    deleteStage: async(id) => {
+        try {
+            return await stageRepository.deleteStage(id);
+        } catch(err) {
+            console.error(`Erro ao deletar palco com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    }
+}
