beac7ce1370195a9ac0148bc5893cf83948e474b
bytemeyu <bytemeyu@gmail.com>
2024-05-28 20:34:17 -0300

    atualizando app.js com rotas stage

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import artistRoutes from './routes/artistRoutes.js';
+import stageRoutes from './routes/stageRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/artist', artistRoutes);
+app.use('api/stage', stageRoutes);
 
 export default app;
