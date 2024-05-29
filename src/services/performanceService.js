ee7b837e98403388f8997ff7e76118dbdd877b21
bytemeyu <bytemeyu@gmail.com>
2024-05-29 11:22:54 -0300

    criando e implementando métodos de performanceService.js

---
+++ src/services/performanceService.js
@@ -0,0 +1,48 @@
+import { performanceRepository } from "../repositories/performanceRepository.js";
+
+export const performanceService = {
+    getAllPerformances: async() => {
+        try {
+            return await performanceRepository.getAllPerformances();
+        } catch(err) {
+            console.error(`Erro ao recuperar todas as performances: ${err.message}`);
+            throw err;
+        }
+    },
+
+    getPerformanceById: async(id) => {
+        try {
+            return await performanceRepository.getPerformanceById(id);
+        } catch(err) {
+            console.error(`Erro ao recuperar performance com id ${id}: ${err.message}`);
+            throw err;
+        }
+    },
+
+    createPerformance: async(artist_id, stage_id, start_time, end_time, date) => {
+        try {
+            return await performanceRepository.createPerformance(artist_id, stage_id, start_time, end_time, date);
+        } catch(err) {
+            console.error(`Erro ao criar nova performance no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    updatePerformance: async(id, artist_id, stage_id, start_time, end_time, date) => {
+        try {
+            return await performanceRepository.updatePerformance(id, artist_id, stage_id, start_time, end_time, date);
+        } catch(err) {
+            console.error(`Erro ao atualizar performance com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    deletePerformance: async(id) => {
+        try {
+            return await performanceRepository.deletePerformance(id);
+        } catch(err) {
+            console.error(`Erro ao deletar performance com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    }
+};
