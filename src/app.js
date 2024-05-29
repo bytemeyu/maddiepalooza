bba04c33ec816bbfde1b73e6693bbb6171ede831
bytemeyu <bytemeyu@gmail.com>
2024-05-29 19:02:04 -0300

    atualizando app.js com rotas performance

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import artistRoutes from './routes/artistRoutes.js';
 import stageRoutes from './routes/stageRoutes.js';
+import performanceRoutes from './routes/performanceRoutes.js';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/api/artist', artistRoutes);
 app.use('/api/stage', stageRoutes);
+app.use('/api/performance', performanceRoutes);
 
 export default app;
