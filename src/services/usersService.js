a9b3a40e7098892af1a2c426788f487e79075aeb
bytemeyu <bytemeyu@gmail.com>
2024-05-29 19:27:36 -0300

    criando e implementando métodos em usersService.js

---
+++ src/services/usersService.js
@@ -0,0 +1,48 @@
+import { usersRepository } from "../repositories/usersRepository.js";
+
+export const usersService = {
+    getAllUsers: async() => {
+        try {
+            return await usersRepository.getAllUsers();
+        } catch(err) {
+            console.error(`Erro ao recuperar todos os usuários: ${err.message}`);
+            throw err;
+        }
+    },
+
+    getUserById: async(id) => {
+        try {
+            return await usersRepository.getUserById(id);
+        } catch(err) {
+            console.error(`Erro ao recuperar usuário com id ${id}: ${err.message}`);
+            throw err;
+        }
+    },
+
+    createUser: async(email, username, password_hash, role) => {
+        try {
+            return await usersRepository.createUser(email, username, password_hash, role);
+        } catch(err) {
+            console.error(`Erro ao criar novo usuário no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    updateUser: async(id, email, username, password_hash, role) => {
+        try {
+            return await usersRepository.updateUser(id, email, username, password_hash, role);
+        } catch(err) {
+            console.error(`Erro ao atualizar usuário com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    },
+
+    deleteUser: async(id) => {
+        try {
+            return await usersRepository.deleteUser(id);
+        } catch(err) {
+            console.error(`Erro ao deletar usuário com id ${id} no banco de dados: ${err.message}`);
+            throw err;
+        }
+    }
+};
