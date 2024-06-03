e4dc20b738d9166f914823f4905d8a4e5ec1e00b
bytemeyu <bytemeyu@gmail.com>
2024-06-03 19:54:38 -0300

    elaborando o authRolesMiddleware.js

---
+++ src/middlewares/authRolesMiddleware.js
@@ -0,0 +1,15 @@
+function authRolesMiddleware(allowedRoles = []) {
+    if (typeof allowedRoles === 'string') {
+        allowedRoles = [allowedRoles];
+        // Garante que allowedRoles seja sempre um array.
+    }
+
+    return (req, res, next) => {
+        if (!allowedRoles.includes(req.user.role)) {
+            return res.status(403).json({ error: 'Acesso negado' });
+        }
+
+        next();
+        // Se o usuário tem um role permitido, chama o próximo middleware. (Se ele não tiver, para na condição acima e não chama um próximo middleware, ou seja, interrompe o acesso a seja lá o que for.)
+    };
+};
