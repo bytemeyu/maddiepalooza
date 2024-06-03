b889bc57239d723793afbeab37fbc1e59b24d938
bytemeyu <bytemeyu@gmail.com>
2024-06-03 20:21:51 -0300

    corrigindo importações para auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,4 @@
-import { authService } from "../services/authService";
+import { authService } from "../services/authService.js";
 
 export const authController = {
     login: async (req, res) => {
--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { usersService } from "./usersService";
+import { usersService } from "./usersService.js";
 
 export const authService = {
     login: async(username, password) => {
