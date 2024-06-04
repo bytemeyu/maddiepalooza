36c1ef37efc442546ee71214de9adc392daef13a
bytemeyu <bytemeyu@gmail.com>
2024-06-03 21:46:09 -0300

    corrigindo a questão dos cookies (simulação do navegador com curl e comando)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -15,8 +15,9 @@ export const authController = {
                 const { id, token } = loginResult;
                 res.cookie('token', token, { 
                     httpOnly: true, 
-                    secure: true,
-                    sameSite: 'strict',
+                    //secure: true,
+                    //sameSite: 'strict',
+                    //Só reativar as opções acima em contexto de produção.
                     maxAge: 3 * 3600000
                 }); 
                 res.status(200).json({ id, token });
@@ -28,7 +29,7 @@ export const authController = {
             res.status(401).json({ error: 'Nome de usuário ou senha inválidos' });
         }
     },
-    //curl -X POST http://localhost:3000/api/auth/login \
+    //curl -c cookies.txt -X POST http://localhost:3000/api/auth/login \
     //-H "Content-Type: application/json" \
     //-d '{
     //"username": "anitakawasaki",
@@ -38,14 +39,16 @@ export const authController = {
     logout: (req, res) => {
         if (req.cookies.token) {
             res.clearCookie('token', {
-                httpOnly: true, 
-                secure: true, 
-                sameSite: 'strict'
+                httpOnly: true 
+                //secure: true, 
+                //sameSite: 'strict'
+                //Só reativar as opções acima em contexto de produção.
             });
             res.status(200).json({ message: 'Logout bem-sucedido' });
         } else {
             res.status(200).json({ message: 'Nenhum token foi encontrado, portanto a sessão já foi encerrada com sucesso' });
         }
     }
-    //curl -X DELETE http://localhost:3000/api/auth/logout
+    //curl -b cookies.txt -X DELETE http://localhost:3000/api/auth/logout
+    //o seguinte comando para limpar cookies.txt manualmente (e simular o logout no navegador): > cookies.txt
   };
