c6015afebd218d621705b23fc4198cda4700dcec
bytemeyu <bytemeyu@gmail.com>
2024-06-06 13:04:39 -0300

    aplicando middleware de autenticação e middleware de tipo de usuário à algumas rotas do artistRoutes.js

--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { artistController } from '../controllers/artistController.js';
+import { authMiddleware } from '../middlewares/authMiddleware.js';
+import { authRolesMiddleware } from '../middlewares/authRolesMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', artistController.getAllArtists);
 router.get('/:id', artistController.getArtistById);
-router.post('/', artistController.createArtist);
-router.put('/:id', artistController.updateArtist);
-router.delete('/:id', artistController.deleteArtist);
+router.post('/', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), artistController.createArtist);
+router.put('/:id', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), artistController.updateArtist);
+router.delete('/:id', authMiddleware, authMiddleware(['webadmin', 'producer']), artistController.deleteArtist);
 
 export default router;
