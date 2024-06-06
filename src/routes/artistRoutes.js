ce3e7348186cab377229d1361466b0e083596e81
bytemeyu <bytemeyu@gmail.com>
2024-06-06 18:15:30 -0300

    corrigindo formatação curl

--- src/routes/artistRoutes.js
+++ src/routes/artistRoutes.js
@@ -11,20 +11,20 @@ router.get('/:id', artistController.getArtistById);
 //curl -X GET http://localhost:3000/api/artist/1
 router.post('/', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), artistController.createArtist);
 //curl -b cookies.txt -X POST http://localhost:3000/api/artist \
-    //-H "Content-Type: application/json" \
-    //-d '{
-    //"name": "Madonna",
-    //"biography": "Biografia da Madonna",
-    //"photo_url": "http://exemplo.com/foto.jpg"
-    //}'
+//-H "Content-Type: application/json" \
+//-d '{
+//"name": "Madonna",
+//"biography": "Biografia da Madonna",
+//"photo_url": "http://exemplo.com/foto.jpg"
+//}'
 router.put('/:id', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), artistController.updateArtist);
-    //curl -b cookies.txt -X PUT http://localhost:3000/api/artist/1 \
-    //-H "Content-Type: application/json" \
-    //-d '{
-    //"name": "Madonna",
-    //"biography": "Biografia da Madonna",
-    //"photo_url": "http://exemplo.com/foto.jpg"
-    //}'
+//curl -b cookies.txt -X PUT http://localhost:3000/api/artist/1 \
+//-H "Content-Type: application/json" \
+//-d '{
+//"name": "Madonna",
+//"biography": "Biografia da Madonna",
+//"photo_url": "http://exemplo.com/foto.jpg"
+//}'
 router.delete('/:id', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), artistController.deleteArtist);
 //curl -b cookies.txt -X DELETE http://localhost:3000/api/artist/2
 
--- src/routes/stageRoutes.js
+++ src/routes/stageRoutes.js
@@ -11,19 +11,19 @@ router.get('/:id', stageController.getStageById);
 //curl -X GET http://localhost:3000/api/stage/1
 router.post('/', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), stageController.createStage);
 //curl -X POST http://localhost:3000/api/stage \
-    //-H "Content-Type: application/json" \
-    //-d '{
-    //"name": "I have no shame",
-    //"location": "Michigan",
-    //"capacity": "750"
-    //}'
+//-H "Content-Type: application/json" \
+//-d '{
+//"name": "I have no shame",
+//"location": "Michigan",
+//"capacity": "750"
+//}'
 router.put('/:id', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), stageController.updateStage);
 //curl -X PUT http://localhost:3000/api/stage/1 \
-    //-H "Content-Type: application/json" \
-    //-d '{
-    //"location": "Michigan/EUA",
-    //"capacity": "650"
-    //}'
+//-H "Content-Type: application/json" \
+//-d '{
+//"location": "Michigan/EUA",
+//"capacity": "650"
+//}'
 router.delete('/:id', authMiddleware, authRolesMiddleware(['webadmin', 'producer']), stageController.deleteStage);
 //curl -X DELETE http://localhost:3000/api/stage/2
 
