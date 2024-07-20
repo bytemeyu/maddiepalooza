c4bba0ecb14bd716edaa80425982ff72c03b4376
bytemeyu <bytemeyu@gmail.com>
2024-07-20 12:42:53 -0300

    criando o Dockerfile para construir a imagem do serviço (container) de frontend no Docker, adicionando o serviço frontend ao docker-compose.yml, configurando o vite.config.js para aceitar conexões de qualquer host

--- frontend/maddiepalooza-vite/vite.config.js
+++ frontend/maddiepalooza-vite/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0'
+  }
+  //Certifique-se de que o Vite está configurado para aceitar conexões de qualquer host, o que é especialmente importante quando se trabalha com containers Docker. Isso pode ser feito ajustando a configuração server.host no arquivo vite.config.js para '0.0.0.0'.
 })
