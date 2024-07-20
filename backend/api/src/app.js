c03fa789e0ff434b379dc572d3c7e680f255d510
bytemeyu <bytemeyu@gmail.com>
2024-07-20 13:24:40 -0300

    configurando política de cors na api

--- backend/api/src/app.js
+++ backend/api/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import artistRoutes from './routes/artistRoutes.js';
 import stageRoutes from './routes/stageRoutes.js';
 import performanceRoutes from './routes/performanceRoutes.js';
@@ -8,6 +9,10 @@ import authRoutes from './routes/authRoutes.js'
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser());
 
