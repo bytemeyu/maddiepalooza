ae4744a1e8b72321c123ab2dad279c2a3d86cb08
bytemeyu <bytemeyu@gmail.com>
2024-09-03 15:01:26 -0300

    começando a criar os testes de integração

---
+++ backend/api/tests/integration/artist.integration.test.js
@@ -0,0 +1,8 @@
+import request from "supertest";
+import app from "../../src/app.js";
+
+describe("artistRoutes", () => {
+  describe("GET /api/artist (getAllArtists)", () => {
+    describe("");
+  });
+});
